39d477588aa0d0a4af9a85820ce7a04232f5a059
anneline-codes <lineanne65@gmail.com>
2026-06-02 21:46:25 +0200

    Some changes made

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
-import { Router } from 'express' ;
-import { getDashboard } from '../controllers/dashboardController.js';
+import { Router } from 'express';
+import { getDashboard } from '../controllers/dashboardController.js' ;
 import { requireAuth } from '../middleware/auth.js';
 
 const router = Router();
