10bbfc718e203cf2529184c801942b494810aa20
anneline-codes <lineanne65@gmail.com>
2026-06-02 21:41:50 +0200

    Some changes made

--- server/seed.js
+++ server/seed.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import User from './models/User.js';
-import Order from './models/Order.js';
+import Order from './models/Order.js' ;
 import MenuItem from './models/MenuItem.js';
 import Client from './models/Client.js';
 import DashboardMetric from './models/DashboardMetric.js';
